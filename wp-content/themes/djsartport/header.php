3486b58fd63da91648b3f30aa16124a23d67afd9
kaiyu li <krisfk@gmail.com>
2024-04-30 05:15:39 +0800

    .

--- wp-content/themes/djsartport/header.php
+++ wp-content/themes/djsartport/header.php
@@ -28,6 +28,22 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
+    <script type="text/javascript">
+    $(function() {
+
+
+        $('.course-tag-ul li a').click(function(e) {
+
+            e.preventDefault();
+
+            $('.course-tag-ul li a').removeClass('active');
+            $(this).addClass('active');
+
+
+        })
+
+    })
+    </script>
 </head>
 
 <body <?php body_class(); ?>>
