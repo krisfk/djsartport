97685da753aa81305d780790bc4bc3125c06aca8
kaiyu li <krisfk@gmail.com>
2024-04-30 06:06:56 +0800

    .

--- wp-content/themes/djsartport/index.php
+++ wp-content/themes/djsartport/index.php
@@ -83,7 +83,7 @@ get_header(); ?>
         <li><a href="javascript:void(0);">證書課程</a></li>
 
     </ul>
-    <div class="text-center small random-text">: : 隨機先後次序排列 : :
+    <div class="text-center small random-text">:: 隨機先後次序排列 ::
     </div>
 
 
