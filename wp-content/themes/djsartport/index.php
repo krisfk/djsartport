1e8299b25d0a6661778963b7a6dc7ef7ac36c6a3
kaiyu li <krisfk@gmail.com>
2024-04-30 04:43:06 +0800

    .

--- wp-content/themes/djsartport/index.php
+++ wp-content/themes/djsartport/index.php
@@ -43,6 +43,8 @@ get_header(); ?>
             <li><a class="level-1  " href="<?php echo get_site_url();?>">主頁</a></li>
             <li><a class="level-1  " href="javascript:void(0);">關於我們</a></li>
             <li><a class="level-1  " href="javascript:void(0);">寄賣招募</a></li>
+            <li><a class="level-1  " href="javascript:void(0);">聯絡我們</a></li>
+
 
 
         </ul>
