78295b5574e5741fa306604ea1153ebe6b38d6b0
kaiyu li <krisfk@gmail.com>
2024-04-30 04:29:25 +0800

    .

--- wp-content/themes/djsartport/index.php
+++ wp-content/themes/djsartport/index.php
@@ -35,7 +35,10 @@ get_header(); ?>
 <div class="container h1-outer-div text-center mt-4">
 
     <h1 class="">
-        <span class="text">興趣班 Interest class</span>
+        <span class="text-div">
+            <div class="text-div-bg">
+                興趣班 Interest class</div>
+        </span>
     </h1>
 </div>
 
