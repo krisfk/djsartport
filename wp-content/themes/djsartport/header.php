6dc675e06ee18eab518108304d7c8ec4a87a3976
kaiyu li <krisfk@gmail.com>
2024-04-30 06:48:02 +0800

    .

--- wp-content/themes/djsartport/header.php
+++ wp-content/themes/djsartport/header.php
@@ -44,7 +44,7 @@
         $('h1').animate({
             'width': '100%',
             'opacity': '1'
-        }, 1000, function() {
+        }, 800, function() {
 
 
             for (i = 0; i < $('.course-tag-ul li').length; i++) {
