02767fd6ff54837811ede20ece0cb4d8fd26e762
kaiyu li <krisfk@gmail.com>
2024-04-30 05:17:28 +0800

    .

--- wp-content/themes/djsartport/index.php
+++ wp-content/themes/djsartport/index.php
@@ -81,6 +81,8 @@ get_header(); ?>
         <li><a href="javascript:void(0);">心理班</a></li>
         <li><a href="javascript:void(0);">音樂班</a></li>
     </ul>
+    <div class="text-center small">隨機先後次序排列...
+    </div>
 
 
 
