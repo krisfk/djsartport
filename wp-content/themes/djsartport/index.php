39c423200449655116452d68aab080f9c445497c
kaiyu li <krisfk@gmail.com>
2024-05-02 00:40:35 +0800

    .

--- wp-content/themes/djsartport/index.php
+++ wp-content/themes/djsartport/index.php
@@ -16,43 +16,6 @@
 
 get_header(); ?>
 
-<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
-<header class="page-header alignwide">
-    <h1 class="page-title"><?php single_post_title(); ?></h1>
-</header><!-- .page-header -->
-<?php endif; ?>
-
-
-<div class="mt-4">
-    <div class="text-center ">
-        <img class="header-logo" src="<?php echo get_template_directory_uri();?>/assets/images/logo.png" alt="">
-    </div>
-</div>
-
-
-
-<div class="container position-relative mt-4">
-    <ul class="top-menu-ul-outer-ul ">
-
-        <ul class=" top-menu-ul">
-
-            <li><a class="level-1  " href="<?php echo get_site_url();?>">主頁</a></li>
-            <li><a class="level-1  " href="javascript:void(0);">關於我們</a></li>
-
-            <li><a class="level-1  " href="javascript:void(0);">興趣班課程</a></li>
-
-            <li><a class="level-1  " href="javascript:void(0);">寄賣招募</a></li>
-
-            <li><a class="level-1  " href="javascript:void(0);">店鋪位置</a></li>
-
-
-            <li><a class="level-1  " href="javascript:void(0);">聯絡我們</a></li>
-
-
-
-        </ul>
-    </ul>
-</div>
 
 
 <div class="container h1-outer-div text-center mt-5">
