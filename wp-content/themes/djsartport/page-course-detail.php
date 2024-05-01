8bdec796e5fed4ae8061f7e22ff6ed158974a7f4
kaiyu li <krisfk@gmail.com>
2024-05-02 04:49:30 +0800

    .

--- wp-content/themes/djsartport/page-course-detail.php
+++ wp-content/themes/djsartport/page-course-detail.php
@@ -70,7 +70,7 @@ get_header(); ?>
 
 
 
-        <div class="col-6">
+        <div class="col-6 pe-3">
 
             <div>
                 <h2>簡介</h2>
@@ -85,7 +85,7 @@ get_header(); ?>
 
         </div>
 
-        <div class="col-6">
+        <div class="col-6 ps-3">
 
 
 
