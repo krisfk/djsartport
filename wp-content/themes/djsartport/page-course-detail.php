01a623396eeeecba19169b3288e586c096208c71
kaiyu li <krisfk@gmail.com>
2024-05-02 00:28:39 +0800

    .

---
+++ wp-content/themes/djsartport/page-course-detail.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header(); ?>
+
+<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
+<header class="page-header alignwide">
+    <h1 class="page-title"><?php single_post_title(); ?></h1>
+</header><!-- .page-header -->
+<?php endif; ?>
+
+
+<div class="mt-4">
+    <div class="text-center ">
+        <img class="header-logo" src="<?php echo get_template_directory_uri();?>/assets/images/logo.png" alt="">
+    </div>
+</div>
+
+
+
+<div class="container position-relative mt-4">
+    <ul class="top-menu-ul-outer-ul ">
+
+        <ul class=" top-menu-ul">
+
+            <li><a class="level-1  " href="<?php echo get_site_url();?>">主頁</a></li>
+            <li><a class="level-1  " href="javascript:void(0);">關於我們</a></li>
+
+            <li><a class="level-1  " href="javascript:void(0);">興趣班課程</a></li>
+
+            <li><a class="level-1  " href="javascript:void(0);">寄賣招募</a></li>
+
+            <li><a class="level-1  " href="javascript:void(0);">店鋪位置</a></li>
+
+
+            <li><a class="level-1  " href="javascript:void(0);">聯絡我們</a></li>
+
+
+
+        </ul>
+    </ul>
+</div>
+
+
+
+
+<?php
+
+get_footer();
