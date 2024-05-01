af7b6dc5b431b4ac24ccb8176dce31d5a9df48b5
kaiyu li <krisfk@gmail.com>
2024-05-02 03:32:08 +0800

    .

--- wp-content/themes/djsartport/page-course-detail.php
+++ wp-content/themes/djsartport/page-course-detail.php
@@ -22,7 +22,11 @@ get_header(); ?>
     <div class="course-detail-banner-div-1"
         style="background: url(http://143.198.107.5/wp-content/uploads/2024/05/WhatsApp-Image-2024-04-20-at-22.45.34.jpeg) center center no-repeat; width: 50%; background-position: center; background-size: 100%;">
     </div>
-    <div class="course-detail-banner-div-2"></div>
+    <div class="course-detail-banner-div-2"
+        style="background: url(http://143.198.107.5/wp-content/uploads/2024/05/WhatsApp-Image-2024-04-20-at-22.45.34.jpeg) center center no-repeat; width: 50%; background-position: center; background-size: 100%;">
+
+
+    </div>
 
 
 </div>
