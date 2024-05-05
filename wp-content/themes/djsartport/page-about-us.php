3edd5e7a3f516ee25478879cac3176ca29ce3b83
kaiyu li <krisfk@gmail.com>
2024-05-05 17:52:12 +0800

    .

--- wp-content/themes/djsartport/page-about-us.php
+++ wp-content/themes/djsartport/page-about-us.php
@@ -20,21 +20,12 @@ get_header(); ?>
 <div class="course-detail-banner-div">
 
 
-    <table>
-        <tr>
-            <td>
-                <h2 class="h2-course-name">日本Aromatics Labo木下和美氨基酸寶石皂 (興趣班)</h2>
-            </td>
-        </tr>
-    </table>
 
     <div class="course-detail-banner-div-1"
         style="background: url(http://143.198.107.5/wp-content/uploads/2024/05/WhatsApp-Image-2024-04-20-at-22.45.34.jpeg) center center no-repeat; width: 50%; background-position: center; background-size: 100%;">
     </div>
     <div class="course-detail-banner-div-2"
         style="background: url(http://143.198.107.5/wp-content/uploads/2024/05/Screenshot-2024-05-02-at-1.12.38 AM.png) center center no-repeat; width: 50%; background-position: center; background-size: 100%;">
-
-
     </div>
 
 
