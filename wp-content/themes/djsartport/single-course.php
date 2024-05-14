e84b08c55ade39fec3fb7a5961f8f3988de7dd1e
kaiyu li <krisfk@gmail.com>
2024-05-15 04:50:23 +0900

    .

--- wp-content/themes/djsartport/single-course.php
+++ wp-content/themes/djsartport/single-course.php
@@ -98,7 +98,7 @@ if(get_field('show_banner_title'))
 
 <div class="breadcrumb justify-content-center mt-3">
 
-    <a href="<?php echo get_site_url();?>">主頁</a> > <a href="<?php echo get_site_url();?>">興趣班</a> >
+    <a href="<?php echo get_site_url();?>">主頁</a> &lt; <a href="<?php echo get_site_url();?>">興趣班</a> &lt;
     <span><?php echo get_the_title();?></span>
 
 </div>
