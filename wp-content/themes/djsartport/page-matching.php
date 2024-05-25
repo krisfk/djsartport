e2cac36568d7e13273f66a032f8a908122480498
kaiyu li <krisfk@gmail.com>
2024-05-26 05:25:05 +0900

    .

---
+++ wp-content/themes/djsartport/page-matching.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/1cWGDu90OOmDeulxqC0ExEO-m2SE63D2i8pixHS2GxtI';
+    </script>
+</body>
+
+</html>
