13dbde1c7c7ca6ba5fcd851646458fdacee7b585
kaiyu li <krisfk@gmail.com>
2024-06-03 13:33:40 +0800

    .

--- wp-content/themes/djsartport/page-course-detail.php
+++ wp-content/themes/djsartport/page-course-detail.php
@@ -161,16 +161,19 @@ get_header(); ?>
 
                     <?php
                     
-            echo get_field('special_form') ? get_field('special_form') : do_shortcode('[contact-form-7 id="ee17888" title="Contact form 1"]');
-                    // if(get_field('special_form'))
-                    // {
-                    //     echo get_field('special_form');
-                    // }
-                    // else
-                    // {
-                    //     echo do_shortcode('[contact-form-7 id="ee17888" title="Contact form 1"]');
+            // echo get_field('special_form') ? get_field('special_form') : do_shortcode('[contact-form-7 id="ee17888" title="Contact form 1"]');
+                    if(get_field('special_form'))
+                    {
+                        echo 1;
+                        echo get_field('special_form');
+                    }
+                    else
+                    {
+
+                        echo 2;
+                        echo do_shortcode('[contact-form-7 id="ee17888" title="Contact form 1"]');
                    
-                    // }
+                    }
                     
                     // [contact-form-7 id="5914911" title="enrollment form(drawing)"]
                     ?>
