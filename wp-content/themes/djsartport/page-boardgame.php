e20f54edd142fd5f5c6f1eac796c94597a853896
kaiyu li <krisfk@gmail.com>
2024-07-04 22:47:17 +0800

    .

---
+++ wp-content/themes/djsartport/page-boardgame.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'docs.google.com/forms/d/1j-ALnXrXwRzN-VxRP4lzS29OVy8rIA9sb5pYmAWJd_g/viewform';
+    </script>
+</body>
+
+</html>
