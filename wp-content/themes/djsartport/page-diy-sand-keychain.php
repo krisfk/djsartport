da45a14e165b79571d3552aeb029f17ab8dd8ab4
Joey <krisfk@gmail.com>
2024-07-10 05:26:30 +0800

    .

---
+++ wp-content/themes/djsartport/page-diy-sand-keychain.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://djsartport.com/course/diy%e6%b2%99%e6%bc%8f%e7%95%ab%e9%8e%96%e5%8c%99%e6%89%a3workshop%e5%bf%83%e6%b5%81%e6%b2%99%e7%95%ab%e8%97%9d%e8%a1%93%e5%ad%b8%e9%99%a2/';
+    </script>
+</body>
+
+</html>
