93099c9e55b0bdb0110fabadb4d61653003b9de1
Joey <krisfk@gmail.com>
2024-07-10 05:23:11 +0800

    .

---
+++ wp-content/themes/djsartport/page-sand-art.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://djsartport.com/course/%e6%b2%99%e7%95%ab%e9%ab%94%e9%a9%97%e7%8f%ad%e5%bf%83%e6%b5%81%e6%b2%99%e7%95%ab%e8%97%9d%e8%a1%93%e5%ad%b8%e9%99%a2/';
+    </script>
+</body>
+
+</html>
