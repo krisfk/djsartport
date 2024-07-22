ac058da9add2f1609a71eba3176cd708584cf8a4
Joey <krisfk@gmail.com>
2024-07-23 04:46:51 +0800

    .

---
+++ wp-content/themes/djsartport/page-spadequeen.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/1bgr73SlAgmU0wPIR0XlksHlanjMUlyNLK3M10_iK2NA/';
+    </script>
+</body>
+
+</html>
