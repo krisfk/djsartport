a68fef1a31bdb8131caa3d46fbd3173a19749d46
Joey <krisfk@gmail.com>
2024-08-01 02:05:29 +0900

    .

---
+++ wp-content/themes/djsartport/page-friday.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/1PTxa5-tlbK9wP76YzAYYf4e8NFVFc7ImXHHVYo9Jo3o/';
+    </script>
+</body>
+
+</html>
