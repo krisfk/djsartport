62d6d0b6ec179a0a6e15a9e8df793a6849bed2b5
Joey <krisfk@gmail.com>
2024-08-06 13:35:33 +0900

    .

---
+++ wp-content/themes/djsartport/page-roller.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/15gESHGutEZXTrQIAVL1obyCvI7H95S3HOeY7cUCoFzg/';
+    </script>
+</body>
+
+</html>
