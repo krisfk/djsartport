6ea5e8b12d9c56afabd68d9d7ca353bed6296d01
Joey <krisfk@gmail.com>
2024-08-26 05:52:56 +0800

    .

---
+++ wp-content/themes/djsartport/page-iching.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLSdI4A3GXHhuu11ErqN4poMPHfpROn6IhRdv3He6EX-_Ah9CmQ/viewform';
+    </script>
+</body>
+
+</html>
