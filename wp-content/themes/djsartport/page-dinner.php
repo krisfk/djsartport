b2e1f33296c2fa27b53c67b00b12e6693d57eca1
Joey <krisfk@gmail.com>
2024-08-27 14:10:51 +0800

    .

---
+++ wp-content/themes/djsartport/page-dinner.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLSd2FqwkFam--3-NJ8BiD3t5bL17pRtjxApquQy_jBdeYQCCnA/viewform';
+    </script>
+</body>
+
+</html>
