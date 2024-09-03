cae5cbe20b62431a5c7e74a1018f9330cc5c7c9d
Joey <krisfk@gmail.com>
2024-09-04 04:38:13 +0800

    .

---
+++ wp-content/themes/djsartport/page-jimbook.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLSe0YEl6FYqsmTbcDs6sLdNzjveneVZ1ebYEPISMoRWjJ6A_cA/viewform';
+    </script>
+</body>
+
+</html>
