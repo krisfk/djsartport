eb2b953349e0177970d34930e3030c8336edeb5b
Joey <krisfk@gmail.com>
2024-09-05 20:54:24 +0800

    .

---
+++ wp-content/themes/djsartport/page-slashdinner.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLSc9npon3-v4EWYYC3uJewNNSnwUA7K3Wwq1H3T82ECLUvyH4A/viewform';
+    </script>
+</body>
+
+</html>
