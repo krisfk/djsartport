73df855ddf7596000a8c4df26f20877d6f15355f
Joey <krisfk@gmail.com>
2024-09-14 21:21:07 +0800

    .

---
+++ wp-content/themes/djsartport/page-impact-force.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLSf9p71PmXolAUJY0Lni9mq40CONXm81yhRBhU8EKCB7KDTgRg/viewform';
+    </script>
+</body>
+
+</html>
