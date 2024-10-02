a3284283453c947dfd3897eaacc3617d6d380975
Joey <krisfk@gmail.com>
2024-10-03 03:08:29 +0900

    .

---
+++ wp-content/themes/djsartport/page-escape.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLSd5g5WJ98qoL-4PxU-3652gYULtMjKx_kUi53JtCJzOrSxXzg/viewform';
+    </script>
+</body>
+
+</html>
