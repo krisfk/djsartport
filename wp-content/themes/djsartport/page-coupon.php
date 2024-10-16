ec98aa0858e2e5af1887ea48233a6c2e006b893c
Joey <krisfk@gmail.com>
2024-10-16 11:57:42 +0800

    .

---
+++ wp-content/themes/djsartport/page-coupon.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLSdHJOm0OfONhg6OXmcUleymc4_jnjThn3IicEhDiu01IeuR_w/viewform';
+    </script>
+</body>
+
+</html>
