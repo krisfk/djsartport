b09993881d9df777892e09804293ac1bdaaab5c3
Joey <krisfk@gmail.com>
2024-10-25 17:15:26 +0800

    .

---
+++ wp-content/themes/djsartport/page-tenfinger.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLSdmxRE1irWw_gksuZXLuc1w9Xzncwg_6_5CcgG2aY1h1B2GSA/viewform';
+    </script>
+</body>
+
+</html>
