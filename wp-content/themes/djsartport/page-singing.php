7514dedac2a665b11db1cd49d42515bc2d508314
Joey <krisfk@gmail.com>
2024-11-19 03:13:45 +0800

    .

---
+++ wp-content/themes/djsartport/page-singing.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLScX5FHeHjzMv3Xhv783WKRHUeavVJBw4OuKmE2gBKG_R9jF4g/viewform';
+    </script>
+</body>
+
+</html>
