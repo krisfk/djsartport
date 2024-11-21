6a401d77395cd56e571e32eb626e24e2b9d1646a
Joey <krisfk@gmail.com>
2024-11-22 01:44:46 +0800

    .

---
+++ wp-content/themes/djsartport/page-christmas-glass-box.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLSczRzH8-Pa4dVNnu6-H0hOHwoz4wXJ-K854Ab6gTPynypPumQ/viewform';
+    </script>
+</body>
+
+</html>
