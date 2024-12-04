4d5f1ad3ad558f1852ed5d918f30c94c31add6aa
Joey <krisfk@gmail.com>
2024-12-05 03:43:22 +0800

    .

---
+++ wp-content/themes/djsartport/page-longboard.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLSeYcr_e1ijCuhIy5-4XNh8nLFyDoKLt8-3hgifEJbY2rzKSYA/viewform';
+    </script>
+</body>
+
+</html>
