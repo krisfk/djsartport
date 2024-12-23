b66a75e887313b82ea8199aeb0a833827f1cb83c
Joey <krisfk@gmail.com>
2024-12-23 16:53:37 +0800

    .

---
+++ wp-content/themes/djsartport/page-tie-dye.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLSeKPVd6-EXXXwJYKbOKgkcbZCHybILg36ZQqgKl-2s2V1lweg/viewform';
+    </script>
+</body>
+
+</html>
