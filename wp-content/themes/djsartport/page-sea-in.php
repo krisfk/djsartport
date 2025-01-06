2b6dadca5944c89233ae3ce6128336c45462a230
Joey <krisfk@gmail.com>
2025-01-07 00:55:27 +0800

    .

--- wp-content/themes/djsartport/page-sea-in.php
+++ wp-content/themes/djsartport/page-sea-in.php
@@ -20,7 +20,7 @@
 
 <div class="container main-container g-0">
 
-<video src="https://djsartport.com/sea.mp4" autoplay loop muted playsinline></video>
+<video src="https://djsartport.com/sea.mp4" autoplay playsinline></video>
 </div>
 </body>
 </html>
