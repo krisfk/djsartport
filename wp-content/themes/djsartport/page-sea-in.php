65b7c7d31d6dd2ca01f2eb9d0461c694c081edfa
Joey <krisfk@gmail.com>
2025-01-08 16:17:51 +0800

    .

--- wp-content/themes/djsartport/page-sea-in.php
+++ wp-content/themes/djsartport/page-sea-in.php
@@ -39,8 +39,9 @@
 </ul>
 
 
-請輸入你的whatsapp號碼 <br>
-(我們24小時內會send結果給你)：
+請輸入你的whatsapp號碼
+<div class="small">(我們24小時內會send結果給你)：</div>
+
 <input type="text" name="" class="form-control mt-2 form-wts" >
 
 </div>
