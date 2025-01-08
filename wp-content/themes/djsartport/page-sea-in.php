efffed7c9fe387482c21421ea92d624ae36d5aea
Joey <krisfk@gmail.com>
2025-01-08 17:18:52 +0800

    .

--- wp-content/themes/djsartport/page-sea-in.php
+++ wp-content/themes/djsartport/page-sea-in.php
@@ -22,7 +22,7 @@
 <div class="sea-lightbox text-center">
 
 <a href="javascript:void(0);" class="close-btn-a">
-    <img src="https://djsartort.com/close-btn.png" alt="">
+    <img src="https://djsartport.com/close-btn.png" alt="">
     </a>
 <h1 class="mt-5">B: 隨意撈起</h1>
 <div class="small">注意：必須完成  <a href="https://docs.google.com/forms/d/e/1FAIpQLSePBOvhS8M3zWv1Ex0HSjDYBx3gF5jg-mix8fmGyREw0-2uuA/viewform" target="_blank">[A:製作飄流瓶]</a> 才可參加這活動</div>
