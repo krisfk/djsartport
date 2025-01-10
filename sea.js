336c619284184236261dcfb972456cf260c038a5
Joey <krisfk@gmail.com>
2025-01-11 04:37:23 +0800

    .

--- sea.js
+++ sea.js
@@ -39,10 +39,10 @@ $(function(){
         $.ajax({
             type: 'POST',
             url: 'https://djsartport.com/wp-json/api/search-wts',
-            dataType: "json",
+            // dataType: "json",
             // contentType: "application/json",
             data: {'wts': form_wts,'find_gender':find_gender},
-            dataType: 'json', //**** REMOVE THIS LINE ****//
+            // dataType: 'json', //**** REMOVE THIS LINE ****//
             cache: false,
             success: function (found)
             {
