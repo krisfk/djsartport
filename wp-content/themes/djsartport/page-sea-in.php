dc9057205320fe6351ecef0836d335e7ee1f769e
Joey <krisfk@gmail.com>
2025-01-12 02:54:41 +0800

    .

--- wp-content/themes/djsartport/page-sea-in.php
+++ wp-content/themes/djsartport/page-sea-in.php
@@ -60,7 +60,7 @@
             </div>
 
             <h6>我們會在24小時內聯絡你打開瓶(<span class="bottle-idx"></span>)的內容。<br>
-               <span class="small">如你在一星期內已抽多過一次，此抽將當作無效。</span>
+               <span class="small">(如你在一星期內已抽多過一次，此抽將當作無效。)</span>
         </h6>
         </div>
 
