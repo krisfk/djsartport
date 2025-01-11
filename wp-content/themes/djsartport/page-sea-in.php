af2f022cbdf9c67def5cb565a5ed06af323edc31
Joey <krisfk@gmail.com>
2025-01-12 02:40:52 +0800

    .

--- wp-content/themes/djsartport/page-sea-in.php
+++ wp-content/themes/djsartport/page-sea-in.php
@@ -50,12 +50,12 @@
 
         <h4 class="mb-3">你抽中了：</h4>
             <div class="result-bottle result-bottle-1"> 
-                <img class="bottle-img" src="https://djsartport.com/blue-bottle.png" alt="">
+                <img class="bottle-img mb-3" src="https://djsartport.com/blue-bottle.png" alt="">
                 <div>男(<span class="idx"></span>)</div>
             </div>
             
             <div class="result-bottle result-bottle-2">
-                <img class="bottle-img" src="https://djsartport.com/red-bottle.png" alt="">
+                <img class="bottle-img mb-3" src="https://djsartport.com/red-bottle.png" alt="">
                 <div>女(<span class="idx"></span>)</div>
             </div>
             
