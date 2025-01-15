86e7e43adeec3b65b02e965f7e245789aff73801
Joey <krisfk@gmail.com>
2025-01-16 01:54:07 +0900

    .

--- sea.js
+++ sea.js
@@ -29,7 +29,7 @@ $(function(){
         $(this).html('你打開了這個飄流瓶⋯⋯');
         $(this).attr('class','opened-txt');
 
-        $('.secret-div').fadeIn(500);
+        $('.secret-div').animate({'opacity':'1'},200);
         
     })
 
