d77b493caa4d34349de71c54bbdfa4b080a28d08
Joey <krisfk@gmail.com>
2025-01-15 23:44:26 +0900

    .

--- wp-content/themes/djsartport/single-sea_member.php
+++ wp-content/themes/djsartport/single-sea_member.php
@@ -13,71 +13,8 @@
   <style type="text/css"></style>
   <body>
     <div class="container main-container g-0 position-relative">
-      <div class="sea-lightbox text-center d-block">
-        <a href="javascript:void(0);" class="close-btn-a">
-          <img src="https://djsartport.com/close-btn.png" alt="">
-        </a>
-        <h1 class="mt-5">B: 隨意撈起</h1>
-        <div class="small">注意：必須完成 <a href="https://docs.google.com/forms/d/e/1FAIpQLSePBOvhS8M3zWv1Ex0HSjDYBx3gF5jg-mix8fmGyREw0-2uuA/viewform" target="_blank">[A: 製作飄流瓶]</a> 才可參加這活動 </div>
-        <div class="info-block info-block-1">
-          <h4 class="mt-4">想抽的對象性別:</h4>
-          <ul class="gender-ul">
-            <li>
-              <a href="javascript:void(0);" class="gender-a gender-a-1">
-                <img class="bottle-img" src="https://djsartport.com/blue-bottle.png" alt="">
-                <div>男</div>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:void(0);" class="gender-a gender-a-2">
-                <img class="bottle-img" src="https://djsartport.com/red-bottle.png" alt="">
-                <div>女</div>
-              </a>
-            </li>
-          </ul>
-          <div class="small">請輸入你的whatsapp號碼(**省去852/853和space位):</div>
-          <div class="small">(我們會在24小時內會send結果給你)</div>
-          <input type="text" name="" class="form-control mt-2 form-wts">
-          <input type="hidden" name="find_gender" class="find-gender">
-          <button class="submit-btn" role="button">💞 隨機一抽 🫶🏻</button>
-          <div class="small mt-2">(一星期只可抽一次)</div>
-          <div class="msg mt-4 text-warning">這個wts號碼沒有用作登記[A: 製作飄流瓶] <br>
-            <a class="text-warning" href="https://docs.google.com/forms/d/e/1FAIpQLSePBOvhS8M3zWv1Ex0HSjDYBx3gF5jg-mix8fmGyREw0-2uuA/viewform" target="_blank">請先完成A步驟</a>(24小時內完成認証)
-          </div>
-        </div>
-
-        <div class="result-block mt-5">
-
-        <h4 class="mb-3">你抽中了:</h4>
-            <div class="result-bottle result-bottle-1"> 
-                <img class="bottle-img mb-3" src="https://djsartport.com/blue-bottle.png" alt="">
-                <div>男(<span class="idx"></span>)</div>
-            </div>
-            
-            <div class="result-bottle result-bottle-2">
-                <img class="bottle-img mb-3" src="https://djsartport.com/red-bottle.png" alt="">
-                <div>女(<span class="idx"></span>)</div>
-            </div>
-
-            <h6>我們會在24小時內聯絡你打開瓶(<span class="bottle-idx"></span>)的內容。<br>
-               <span class="small">(如你在一星期內已抽多過一次，此抽將當作無效。)</span>
-        </h6>
-        </div>
-
-
-
-      </div>
-      <!-- <video src="https://djsartport.com/sea.mp4" autoplay muted playsinline></video> -->
-      <div class="bottom-div">
-        <ul>
-          <li>
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSePBOvhS8M3zWv1Ex0HSjDYBx3gF5jg-mix8fmGyREw0-2uuA/viewform" target="_blank"> A: 製作飄流瓶</a>
-          </li>
-          <li>
-            <a href="javascript:void(0);" class="b-btn"> B: 隨意撈起</a>
-          </li>
-        </ul>
-      </div>
+    
+    test
     </div>
   </body>
 </html>
