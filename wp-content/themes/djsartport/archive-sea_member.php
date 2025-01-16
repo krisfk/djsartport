cebc4972012c08a6cdfbdf0c3c4a626a4d85cf8d
Joey <krisfk@gmail.com>
2025-01-16 13:45:31 +0900

    .

--- wp-content/themes/djsartport/archive-sea_member.php
+++ wp-content/themes/djsartport/archive-sea_member.php
@@ -1,11 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  fds
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>緣份之海</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://djsartport.com/sea2.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://djsartport.com/sea.js"></script>
+  </head>
+  <style type="text/css"></style>
+  <body>
+    <div class="container main-container g-0 position-relative pt-4 pb-4">
+
+  <h1>網址輸入錯誤⋯如有任何問題可whatsapp 51936670查詢。</h1>
+   </div>
+
+    
+    
+ 
+ 
+ 
+     </div>
+
+
 </body>
 </html>
