0db26389615aab58cf6bd70d7e72f4926bbeefdb
Joey <krisfk@gmail.com>
2025-02-01 04:10:03 +0800

    .

--- wp-content/themes/djsartport/page-sea-in.php
+++ wp-content/themes/djsartport/page-sea-in.php
@@ -66,7 +66,7 @@
             <h4 class="mb-3">最後步驟:(發佈結果前..我們會檢查🙏🏻)</h4>
             1. IG follow 我們: <a href="https://www.instagram.com/djs_artport" target="_blank">djs_artport</a> <br>
             2. Share 這個ig post to story (<a href="https://www.instagram.com/p/DE3-ePKSs_i/" target="_blank">連結在此</a>)<br> 
-            <img src="https://djsartport.com/sea-screenshot.jpg" alt="">
+            <img class="w-50 mt-3 mb-3" src="https://djsartport.com/sea-screenshot.jpg" alt="">
              </div>
 
 
