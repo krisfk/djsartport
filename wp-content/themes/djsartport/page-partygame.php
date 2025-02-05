4be30073d8f8de1eedbf4d6f74621848cc8eecec
Joey <krisfk@gmail.com>
2025-02-06 04:39:21 +0800

    .

---
+++ wp-content/themes/djsartport/page-partygame.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLScn4bYJzRIODMLocdxHYQ_dXNCMRiaYCgYCGx75YqCAPo8Iiw/viewform';
+    </script>
+</body>
+
+</html>
