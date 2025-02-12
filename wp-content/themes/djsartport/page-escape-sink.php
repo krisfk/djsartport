1a74e5b798aff9c3b4c528ed12bd460deba439d0
Joey <krisfk@gmail.com>
2025-02-12 20:25:22 +0800

    .

---
+++ wp-content/themes/djsartport/page-escape-sink.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLSdEt3jvpP8DjkJTkx1KJp-dSSWzRMmoH4ZElvQTc9YRbv3GUg/viewform';
+    </script>
+</body>
+
+</html>
