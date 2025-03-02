def51ab4ff305e3236cdbe8e407a936e14ab7878
Joey <krisfk@gmail.com>
2025-03-03 05:55:24 +0800

    .

---
+++ wp-content/themes/djsartport/page-fox.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLSev6bh4r_LDXcOICJTRFW5bproqpf5QG7AYud-_7OZEUQeN1w/viewform';
+    </script>
+</body>
+
+</html>
