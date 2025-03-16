b8c47eed13379ee08640c6534e1cdbfe4d929954
Joey <krisfk@gmail.com>
2025-03-17 02:35:16 +0800

    .

---
+++ wp-content/themes/djsartport/page-paper-folding.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script type="text/javascript">
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLScS1E1BalLyq-6a9z_l1v3wedgEUEggQUIJi5rNanroAHJvVw/viewform';
+    </script>
+</body>
+
+</html>
